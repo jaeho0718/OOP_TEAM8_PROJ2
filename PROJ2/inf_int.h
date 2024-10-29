5293f547f94886929d96c71ee3f7f373cff469b2
YamYamee <82762402+YamYamee@users.noreply.github.com>
2024-10-29 22:09:50 +0900

    Update inf_int.h

--- PROJ2/inf_int.h
+++ PROJ2/inf_int.h
@@ -16,11 +16,11 @@ private:
     // You may modify or add private members of inf_int class. So, it is OK to insert Add() private member function in inf_int class. However, You may not modify public members of inf_int class.
 
 public:
-    inf_int();               // assign 0 as a default value
-    inf_int(int);
-    inf_int(const char*);   // you may modify this to "inf_int(const string);"
-    inf_int(const inf_int&); // copy constructor
-    ~inf_int(); // destructor
+    inf_int();               // assign 0 as a default value 재호형
+    inf_int(int);            // 재호형
+    inf_int(const char*);   // you may modify this to "inf_int(const string);" 재호형
+    inf_int(const inf_int&); // copy constructor 재호형
+    ~inf_int(); // destructor // 재호형
 
     inf_int& operator=(const inf_int&); // assignment operator
 
@@ -30,9 +30,9 @@ public:
     friend bool operator<(const inf_int&, const inf_int&);
 
     friend inf_int operator+(const inf_int&, const inf_int&);
-    friend inf_int operator-(const inf_int&, const inf_int&);
+    friend inf_int operator-(const inf_int&, const inf_int&); // 재호형
     friend inf_int operator*(const inf_int&, const inf_int&);
-    // friend inf_int operator/(const inf_int& , const inf_int&); // not required
+    friend inf_int operator/(const inf_int& , const inf_int&); // 재호형
 
     friend ostream& operator<<(ostream&, const inf_int&);
     // friend istream& operator>>(istream& , inf_int&);    // not required
